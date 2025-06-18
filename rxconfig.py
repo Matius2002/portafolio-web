a48fe9d1ccdc95ece6b83da2904d0ee70b0bcb89
Mateo Alejandro Mora <mateomora2015@gmail.com>
2025-06-17 22:16:36 -0500

    Se comentarea rxconfig.py

--- rxconfig.py
+++ rxconfig.py
@@ -1,5 +1,11 @@
+# ==========================
+# Importación del framework Reflex
+# ==========================
 import reflex as rx
 
+# ==========================
+# Configuración básica de la aplicación Reflex
+# ==========================
 config = rx.Config(
     app_name="portafolio",
 )
