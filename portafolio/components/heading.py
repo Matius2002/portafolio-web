78eb189fdbb1f9512e3a2a644a8755656d52949c
Mateo Alejandro Mora <mateomora2015@gmail.com>
2025-06-17 22:03:14 -0500

    Se comentarea heading.py

--- portafolio/components/heading.py
+++ portafolio/components/heading.py
@@ -1,10 +1,21 @@
+# ==========================
+# Importaciones necesarias
+# ==========================
 import reflex as rx
 from portafolio.styles.styles import Size
 
-
+# ==========================================
+# Componente reutilizable para encabezados
+# ==========================================
 def heading(text: str, h1=False) -> rx.Component:
     return rx.heading(
         text,
+        # ==========================
+        # Nivel del encabezado (h1 o h2)
+        # ==========================
         as_="h1" if h1 else "h2",
+        # ==========================
+        # Tamaño visual del encabezado
+        # ==========================
         size=Size.BIG.value if h1 else Size.MEDIUM.value
     )
