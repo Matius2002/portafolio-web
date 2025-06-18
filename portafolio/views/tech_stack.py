61f87e819859be888c52bc5c424c0860f9821107
Mateo Alejandro Mora <mateomora2015@gmail.com>
2025-06-17 21:56:17 -0500

    Se comentarea tech_stack.py

--- portafolio/views/tech_stack.py
+++ portafolio/views/tech_stack.py
@@ -1,26 +1,43 @@
+# ==========================
+# Importaciones necesarias
+# ==========================
 import reflex as rx
 from portafolio.components.heading import heading
 from portafolio.data import Technology
 from portafolio.styles.styles import EmSize, Size
 
-
+# ==============================================
+# Componente para mostrar el stack tecnológico
+# ==============================================
 def tech_stack(technologies: list[Technology]) -> rx.Component:
     return rx.vstack(
+        # ==========================
+        # Título de la sección
+        # ==========================
         heading("Tecnologías"),
+        # ==========================================
+        # Contenedor flexible para las tecnologías
+        # ==========================================
         rx.flex(
             *[
+                # Representación de cada tecnología como una insignia
                 rx.badge(
+                    # Ícono de la tecnología
                     rx.box(
                         class_name=technology.icon,
                         font_size="24px"
                     ),
+                    # Nombre de la tecnología
                     rx.text(technology.name),
-                    size="2"
+                    size="2" # Tamaño de la insignia
                 )
-                for technology in technologies
+                for technology in technologies # Itera sobre la lista de tecnologías
             ],
-            wrap="wrap",
-            spacing=Size.SMALL.value
+            wrap="wrap", # Permite que los elementos se ajusten a múltiples líneas si es necesario
+            spacing=Size.SMALL.value # Espaciado entre los elementos
         ),
+        # ==========================
+        # Espaciado general del stack
+        # ==========================
         spacing=Size.DEFAULT.value
     )
