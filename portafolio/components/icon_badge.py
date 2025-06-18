6cae16010bbc0e5b3d7f15a0118d51511f9f3fb8
Mateo Alejandro Mora <mateomora2015@gmail.com>
2025-06-17 22:05:12 -0500

    Se comentarea icon_badge.py

--- portafolio/components/icon_badge.py
+++ portafolio/components/icon_badge.py
@@ -1,14 +1,24 @@
+# ==========================
+# Importaciones necesarias
+# ==========================
 import reflex as rx
-
 from portafolio.styles.styles import EmSize
 
-
+# ==========================================
+# Componente reutilizable para mostrar un ícono dentro de una insignia (badge)
+# ==========================================
 def icon_badge(icon: str) -> rx.Component:
     return rx.badge(
+        # ==========================
+        # Ícono dentro de la insignia
+        # ==========================
         rx.icon(
             icon,
             size=32
         ),
+        # ==========================
+        # Estilos de la insignia
+        # ==========================
         aspect_ratio="1",
         variant="soft"
     )
